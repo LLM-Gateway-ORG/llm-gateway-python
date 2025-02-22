827a6e2c11d7a3b4f10e8d279c5808fd2ca3eac3
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-02-22 18:29:45 +0530

    fixed get models function

--- llmgateway/utils.py
+++ llmgateway/utils.py
@@ -3,6 +3,6 @@ import os
 
 
 def get_model_list() -> dict:
-    filename = os.path.join(os.getcwd(), "src/provider/data/models_list.json")
+    filename = os.path.join(os.getcwd(), "models_list.json")
     with open(filename, "r") as f:
         return json.loads(f.read())
