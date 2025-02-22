6dd8f0940cfc432669d63eac09009326196d9abc
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-02-22 19:28:13 +0530

    fixed models list function path issue

--- llmgateway/utils.py
+++ llmgateway/utils.py
@@ -3,6 +3,12 @@ import os
 
 
 def get_model_list() -> dict:
-    filename = os.path.join("models_list.json")
+    # Get the absolute path of the current file (module)
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to models_list.json within the package
+    filename = os.path.join(package_dir, "models_list.json")
+
+    # Read and return JSON data
     with open(filename, "r") as f:
-        return json.loads(f.read())
+        return json.load(f)
